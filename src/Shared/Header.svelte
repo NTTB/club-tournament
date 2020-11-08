94e82896cd5b54ead73b92383d212f75875d8882
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-11-08 13:15:56 +0100

    Add backbutton and menubutton to header

--- src/Shared/Header.svelte
+++ src/Shared/Header.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import FaArrowLeft from "svelte-icons/fa/FaArrowLeft.svelte";
+  import MdMenu from "svelte-icons/md/MdMenu.svelte";
   export let title = "";
-  export let backUrl = "";
+  export let backUrl: string = undefined;
+  export let showMenuButton = true;
 </script>
 
 <style>
@@ -24,17 +26,36 @@
     padding: 8px;
   }
   .title {
+    grid-column-start: 2;
     overflow: hidden;
     white-space: pre;
   }
-  a.back-button{
+  a.back-button {
     color: var(--text-white);
+  }
+
+  button.menu-button {
+    background-color: transparent;
+    color: var(--text-white);
+    border: none;
+    margin: 0;
+    padding: 8px;
+    border-radius: 0;
+    width: 48px;
+    height: 48px;
   }
 </style>
 
 <div class="header">
-  <a href={backUrl} class="back-button icon">
-    <FaArrowLeft />
-  </a>
+  {#if backUrl}
+    <a href={backUrl} class="back-button icon">
+      <FaArrowLeft />
+    </a>
+  {:else if showMenuButton}
+    <button class="menu-button">
+      <MdMenu />
+    </button>
+  {/if}
+
   <div class="title">{title}</div>
 </div>
