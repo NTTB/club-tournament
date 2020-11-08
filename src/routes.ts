7b2b26bd67355c4f84820e427626cb54604f33b9
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-11-08 13:34:24 +0100

    Add modern style routes

--- src/routes.ts
+++ src/routes.ts
@@ -8,14 +8,25 @@ export interface Route {
   component: any;
 }
 
+function mapRoute(url: string): RegExp {
+  return new RegExp("^" + url.replace(/:(\w+)/g, (_, name) => `(?<${name}>\\w+)`));
+}
+
 export const routes: Route[] = [
-  { match: (v) => /^players\/?/.test(v), component: PagePlayers },
+  { match: (v) => mapRoute("/tournament/:id/info").test(v), component: PageTournament },
+  { match: (v) => mapRoute("/tournament/:id/poule").test(v), component: PagePoule },
+  { match: (v) => mapRoute("/tournament/:id/players").test(v), component: PagePlayers },
+
+  // Old routes
   { match: (v) => /^tournament\/?/.test(v), component: PageTournament },
+  { match: (v) => /^players\/?/.test(v), component: PagePlayers },
   { match: (v) => /^poule\/?/.test(v), component: PagePoule },
+  { match: (v) => /^tournaments$/.test(v), component: PageTournamentList },
+
+  // The index page and 404
   { match: (v) => "" == v, component: PageTournamentList },
   { match: _ => true, component: Page404 },
 ];
-
 
 export function getCurrentRoute(route: string): Route {
   return routes.find(v => v.match(route));
