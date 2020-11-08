807e94ae6fb13e756169be68615715590cb91bcd
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-11-08 13:50:33 +0100

    Add smart paths

--- src/routes.ts
+++ src/routes.ts
@@ -1,38 +1,48 @@
 import { derived } from 'svelte/store';
 import { locationStore } from './routing';
+import type { Route } from './route.interface';
 
 import PagePlayers from './Pages/Tournament/Players.svelte';
 import PagePoule from './Pages/Tournament/Poule.svelte';
 import PageTournament from './Pages/Tournament/Info.svelte';
 import PageTournamentList from './Pages/List.svelte';
 import Page404 from './Pages/404.svelte';
-export interface Route {
-  match: (v: string) => boolean,
-  component: any;
-}
 
-function mapRoute(url: string): RegExp {
+
+function smartPath(url: string): RegExp {
   return new RegExp("^" + url.replace(/:(\w+)/g, (_, name) => `(?<${name}>\\w+)`));
 }
 
-export const routes: Route[] = [
-  { match: (v) => mapRoute("/tournament/:id/info").test(v), component: PageTournament },
-  { match: (v) => mapRoute("/tournament/:id/poule").test(v), component: PagePoule },
-  { match: (v) => mapRoute("/tournament/:id/players").test(v), component: PagePlayers },
+const routes: Route[] = [
+  { path: smartPath("/tournament/:id/info"), component: PageTournament },
+  { path: smartPath("/tournament/:id/poule"), component: PagePoule },
+  { path: smartPath("/tournament/:id/players"), component: PagePlayers },
 
   // Old routes
-  { match: (v) => /^tournament\/?/.test(v), component: PageTournament },
-  { match: (v) => /^players\/?/.test(v), component: PagePlayers },
-  { match: (v) => /^poule\/?/.test(v), component: PagePoule },
-  { match: (v) => /^tournaments$/.test(v), component: PageTournamentList },
+  { path: /^tournament\/?/, component: PageTournament },
+  { path: /^players\/?/, component: PagePlayers },
+  { path: /^poule\/?/, component: PagePoule },
+  { path: /^tournaments$/, component: PageTournamentList },
 
   // The index page and 404
-  { match: (v) => "" == v, component: PageTournamentList },
-  { match: _ => true, component: Page404 },
+  { path: "", component: PageTournamentList },
+  { path: _ => true, component: Page404 },
 ];
 
-export function getCurrentRoute(route: string): Route {
-  return routes.find(v => v.match(route));
+function findRoute(route: string) {
+  const foundRoutes = routes.filter(v => {
+    switch (typeof v.path) {
+      case "function": return v.path(route);
+      case "string": return v.path == route;
+      default: return v.path.test(route);
+    }
+  });
+
+  if (foundRoutes.length === 0)
+    console.error(`No route found for ${route}`);
+  if (foundRoutes.length > 2)
+    console.warn(`Multiple routes found for ${route}`);
+  return foundRoutes;
 }
 
 export const currentPageComponent = derived(locationStore, route => {
@@ -41,11 +51,3 @@ export const currentPageComponent = derived(locationStore, route => {
   return foundRoutes[0].component;
 });
 
-function findRoute(route: string) {
-  const foundRoutes = routes.filter(v => v.match(route));
-  if (foundRoutes.length === 0)
-    console.error(`No route found for ${route}`);
-  if (foundRoutes.length > 2)
-    console.warn(`Multiple routes found for ${route}`);
-  return foundRoutes;
-}
