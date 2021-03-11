e0892ca4a58b09a297a0d6c69496029963626a68
Wouter Lindenhof <wouterlindenhof@gmail.com>
2021-03-11 20:55:56 +0100

    Add ratio-display

---
+++ src/Pages/Tournament/Play/_PoolRankingRatio.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let win: number;
+  export let lose: number;
+</script>
+
+<div class="container">
+  <div class="win">{win}</div>
+  <div class="seperator">-</div>
+  <div class="lose">{lose}</div>
+</div>
+
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    gap: 6px;
+  }
+  .seperator {
+    text-align: center;
+  }
+  .win {
+    text-align: right;
+  }
+  .lose {
+    text-align: left;
+  }
+</style>
